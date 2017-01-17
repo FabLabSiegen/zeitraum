27a11ac74a7125407936878c37b84075651002af
MartiniMoe <martini.moe@cheex.de>
2017-01-17 16:11:50 +0100

    fix monitor start script

--- Presentation/start_presentation_monitor.sh
+++ Presentation/start_presentation_monitor.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-soffice "--accept=socket,host=0,port=2002;urp;" --view --nolockcheck --norestore 
-testpräsentation.odp
+soffice "--accept=socket,host=0,port=2002;urp;" --view --nolockcheck --norestore testpräsentation.odp
