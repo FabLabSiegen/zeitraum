f4092b48380e9064fdbfa3016ebf61198f891e62
Grosskopf <gerrit@familiegrosskopf.de>
2018-11-20 11:22:21 +0100

    added comments and made script more reliable, also fixed my mistakes

--- LeonardoScript/src/main.cpp
+++ LeonardoScript/src/main.cpp
@@ -4,9 +4,10 @@
 #include <Time.h>
 #include <Keyboard.h>
 
-int NUM_ADUINO = 1;
-int LAST_SENSOR_PREV = 13;
-int LAST_SENSOR = 13;
+int NUM_ARDUINO = 2;//number of Arduinos before this
+int LAST_SENSOR_PREV = 13;//numbers from 1 to 13 showing how many pins were connected to the leonardo
+int LAST_SENSOR =1;//numbers from 0 to 12, last pin that something is connected to.
+//ATTENTION: Pin 13 is not usable easily as an input, an LED is connected to it, therefore it will not be used
 
 
 void setup() {
@@ -16,22 +17,13 @@ void setup() {
 }
 
 void sendKeyboardAction(int i) {
-    int valuetopost=(NUM_ADUINO * (LAST_SENSOR_PREV+1)) + LAST_SENSOR-i;//reading from highest to lowest
-    Serial.print("Sending "); Serial.println(valuetopost);
-    if(valuetopost>9){
-      valuetopost+=97-9;
-      char chartopost=valuetopost;
-      Keyboard.write(chartopost);
-    }
-    else{
-      Keyboard.write(valuetopost);
-    }
-    //Keyboard.write(KEY_RETURN);
+    Keyboard.print((NUM_ARDUINO * 13) + (12-i)); //there should not be a last_sensor_prev here, if different amount of pins are used on different arduinos this goes to sh*ts
+    Keyboard.write(KEY_RETURN);
 }
 
 int touchedPort = -1;
 bool touched = false;
-time_t touchTime = time_t(0.5);
+time_t touchTime = time_t(0.25);
 time_t touchTimestamp = 0.0f;
 void touchAction(int i) {
     if (!touched) {
@@ -42,7 +34,7 @@ void touchAction(int i) {
         Serial.print("New touch on "); Serial.println(i);
     } else if (touchedPort == i) {
         if  (now() - touchTimestamp >= touchTime) {
-            // Touch lasted 2 secs
+            // Touch lasted touchTime secs
             touched = false;
             Serial.print("Touch completed on "); Serial.println(i);
             sendKeyboardAction(i);
@@ -56,7 +48,7 @@ void touchAction(int i) {
 
 void loop() {
     bool something_touched = false;
-    for (int i = 0; i < LAST_SENSOR + 1; i++) {
+    for (int i = 12-LAST_SENSOR; i < 13; i++) {//turning arduino arround
         if (digitalRead(i) == 1) {
             touchAction(i);
             something_touched = true;
--- PresentationControllerScript/controller_html.py
+++ PresentationControllerScript/controller_html.py
@@ -1,11 +1,14 @@
 #!/bin/python3
-import subprocess
 
-numLanguages = 3
+#constants
+numLanguages = 3#number after the last language pin number, first pin that shows a slide
 languagePins = [0, 1, 2] # de, en, nl
-language = "de"
-slide = 0
 
+#variables
+language = "de"#current language, is an often changed variable in the code
+slide = 0#current slide, is an often changed variable in the code
+
+#function to rewrite the currentSlide.js file that defines what slide should be shown.
 def writeFile():
 	try:
 		file = open("/home/pi/Desktop/share/zeitraum/Presentation_HTML/currentSlide.js", "w")
@@ -16,25 +19,23 @@ def writeFile():
 		print("Unable to write file.")
 		print(str(Exception))
 
+#loop that goes and goes and goes, hopefully never stopping
 while(True):
 	try:
-		inputNum=subprocess.check_output(["read " "-s " "-n " "1 " "-r " "rep " "&& " "echo " "\"$rep\" "], shell=True)[0]
-		if(inputNum>57):
-			inputNum -=87
-		else:
-			inputNum -=48
-		#reads one buttonpress and only one button, changed the arduinos to buttonpresses to enable gradual changes for direct perception of reaction later down the line
-		if inputNum in languagePins:
-			if inputNum == 0:
-				language = "de"
-			elif inputNum == 1:
-				language = "en"
-			elif inputNum == 2:
-				language = "nl"
-		else:
-			slide = inputNum - numLanguages
-			print("Changing slide to " + str(slide))
-		writeFile()
+		inputString = input("Slide: ")#asks for a number
+		if(inputString): #Checks if the String is empty or not, when two leonardos fire at the same time, say 15 and 7 then this can result in a 157 and an empty line, the empty line made this go boom
+			inputNum = int(inputString)#converts the input to a number, since only numbers are input, this should not crash
+			if inputNum in languagePins:
+				if inputNum == 0:
+					language = "de"
+				elif inputNum == 1:
+					language = "en"
+				elif inputNum == 2:
+					language = "nl"
+			else:
+				slide = inputNum - numLanguages
+				print("Changing slide to " + str(slide))
+			writeFile()
 	except Exception:
 		print(str(sys.exc_info()))
 	pass
